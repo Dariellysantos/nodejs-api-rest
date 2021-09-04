754b72d2eec72d0e669bb75a17610e73ffc4f464
Darielly Santos <daryh_cristina@hotmail.com>
2021-09-03 23:08:22 -0300

    inclui o arquivo gitignore e removi versionamento do node_modules

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const customEpress = require("./config/customExpress");
+const cunstomExpress = require("./config/cunstomExpress");
 
-const app = customEpress();
+const app = cunstomExpress();
 
 // acessar rota _-> dar retorno
 app.listen(3000, () => console.log(" Servidor rodando na porta 3000"));
