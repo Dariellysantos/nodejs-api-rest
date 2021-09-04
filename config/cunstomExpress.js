bc12071769b746a7f1a5b645c05e33fe7b912aca
Darielly Santos <daryh_cristina@hotmail.com>
2021-09-04 00:18:00 -0300

    adicionei o body-parser para conseguir ler os dados que estamos recebendo no body

--- config/cunstomExpress.js
+++ config/cunstomExpress.js
@@ -3,6 +3,8 @@ const consign = require("consign");
 
 module.exports = () => {
   const app = express();
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
 
   consign().include("controllers").into(app);
   return app;
--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -3,7 +3,9 @@ module.exports = (app) => {
     res.send("você está na rota de atendimentos e está realizando um GET")
   );
 
-  app.post("/atendimentos", (req, res) =>
-    res.send("Você está da rota de atendimento e está realizando um POST")
-  );
+  app.post("/atendimentos", (req, res) => {
+    console.log(req.body);
+
+    res.send("Você está da rota de atendimento e está realizando um POST");
+  });
 };
